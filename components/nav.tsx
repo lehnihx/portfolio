dc221b45ff9781ecaa77971640ccb082b1e237b5
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-19 11:32:35 +0100

    style(ui): update color scheme and button hover effects
    - Change nav background from foreground to background color
    - Modify button styles to transparent with scale effect on hover
    - Update nav links cursor style to default
    - Adjust hero section background and text colors for better contrast
    - Fix typo in hero section introduction text
    - Add roadmap section with TODO for locales in README.md

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,14 +3,14 @@ import { Button } from "./ui/button"
 import { X } from "lucide-react"
 
 const ShowNav = ({ setStates }: State<{ nav: boolean }>) =>
-<header className="fixed w-screen h-screen bg-foreground">
+<header className="fixed w-screen h-screen bg-background">
   <Button {...{
-    className: "bg-background text-foreground hover:bg-background/80 absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2",
+    className: "hover:bg-transparent bg-transparent hover:scale-140 text-foreground absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2",
     onClick: () => {setStates(prev => ({ ...prev, nav: !prev.nav }))}
   }}>
     <X/>
   </Button>
-  <nav className="h-screen flex flex-col items-center justify-center text-background text-8xl">
+  <nav className="h-screen flex flex-col items-center justify-center text-foreground text-8xl [&>a]:cursor-default!">
     <a href="#">Home</a>
     <a href="#about">About</a>
     <a href="#projects">Projects</a>
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => 
-<section id="hero" className="h-screen flex flex-col items-center justify-center text-3xl">
-  <h2>Hi, I'm Lenix, and I'm a Junior Fullstack Backend-Focued Software Engineer.</h2>
+<section id="hero" className="h-screen bg-foreground text-background flex flex-col items-center justify-center text-3xl">
+  <h2>Hi, I'm Lenix, and I'm a Junior Fullstack Backend-Focused Software Engineer.</h2>
 </section>
 
 export { Hero }
