450105e8d6abbc5238e8bda9913b1d8e88af48e1
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-19 13:52:10 +0100

    feat(dict): add dictionary context provider and hook
    - Create DictContext with Locales type
    - Implement DictProvider component to supply dictionary context
    - Add useDict hook to access dictionary context in components
    - Enable centralized management of localized strings

---
+++ lib/dict-context.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { Locales } from '@/app/[lang]/dictionaries'
+import { createContext, useContext } from 'react'
+
+const DictContext = createContext<Locales>({} as Locales)
+export const DictProvider = ({ dict, children }: { dict: Locales, children: React.ReactNode }) =>
+  <DictContext.Provider value={dict}>{children}</DictContext.Provider>
+
+export const useDict = () => useContext(DictContext)
