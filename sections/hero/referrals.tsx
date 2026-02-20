af22262c9cfd55a9010ef72a12d49bd10026f643
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-21 00:01:35 +0100

    feat(assets): add fiverr logo SVG to public folder
    - Added new fiverr.svg file with SVG markup for the Fiverr logo
    - Included proper viewBox and fill colors for the SVG graphic
    - Created grouped path elements to define the logo shape and colors

--- sections/hero/referrals.tsx
+++ sections/hero/referrals.tsx
@@ -1,29 +1,45 @@
-import LogoLoop from '@/components/ui/LogoLoop'
-import Image from 'next/image'
-import { SiFiverr } from 'react-icons/si'
-import Grey from '@/public/grey.svg'
+"use client"
 
-const techLogos = [
-  { node: <SiFiverr/>, title: "Fiverr", href: "https://www.fiverr.com/pe/YRNR08z" },
-  { node: <Image src="https://img.alicdn.com/imgextra/i2/O1CN01js79rH1mt5nkV0kEl_!!6000000005011-55-tps-640-180.svg" alt="Qoder" width={100} height={100} />, title: "Qoder", href: "https://qoder.com/referral?referral_code=oyPcpnNF4QB2Rw2tjdglEkanB6w5RpTb" },
-  { node: <Image src={Grey} alt="Grey" width={100} height={100} />, title: "Grey", href: "https://greyapp.page.link/jixhVD5wH6mdykEY7" },
+import { InfiniteSlider } from '@/components/core/infinite-slider'
+import { Dialog } from '@/components/dialog'
+import { useDict } from '@/lib/dict'
+import Image from 'next/image'
+
+const referrals = [
+  {
+    src: '/fiverr.svg',
+    alt: 'Fiverr',
+    url: 'https://www.fiverr.com/pe/YRNR08z',
+  },
+  {
+    src: '/qoder.svg',
+    alt: 'Qoder',
+    url: 'https://qoder.com/referral?referral_code=oyPcpnNF4QB2Rw2tjdglEkanB6w5RpTb',
+  },
+  {
+    src: '/grey.svg',
+    alt: 'Grey',
+    url: 'https://greyapp.page.link/jixhVD5wH6mdykEY7',
+  },
 ]
 
-const Referrals = () => (
-  <div style={{ height: 'fit-content', position: 'relative', overflow: 'hidden' }} className='dark:bg-neutral-900'>
-    <LogoLoop
-      logos={techLogos}
-      speed={10}
-      direction="left"
-      logoHeight={60}
-      gap={60}
-      hoverSpeed={0}
-      scaleOnHover
-      fadeOut
-      fadeOutColor="#ffffff"
-      ariaLabel="Referrals"
-    />
-  </div>
-)
+const Referrals = () => {
+  const dict = useDict()
+
+  return (
+    <InfiniteSlider className='w-full flex items-center justify-center' gap={44}>
+      {referrals.map(({ src, alt, url }) => (
+        <Dialog
+          key={alt}
+          dict={dict}
+          metadata={{ name: alt, url }}
+          additionalClasses='w-[100px] h-[100px]'
+        >
+          <Image src={src} alt={alt} width={100} height={100} className='object-contain' />
+        </Dialog>
+      ))}
+    </InfiniteSlider>
+  )
+}
 
 export { Referrals }
