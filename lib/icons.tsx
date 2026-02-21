2c43825f1659d3886b9e8fc0872229d271640441
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-21 16:26:04 +0100

    build(config): add SVG support with @svgr/webpack loader
    - Add webpack rule to process .svg files using @svgr/webpack
    - Configure Turbopack to handle .svg files with @svgr/webpack loader
    - Export new SVG icons Grey, Qoder, and Fiverr from icons module
    - Add @svgr/webpack dependency to package.json and update lockfile accordingly

---
+++ lib/icons.tsx
@@ -0,0 +1,3 @@
+export { default as Grey } from "@/public/grey.svg"
+export { default as Qoder } from "@/public/qoder.svg"
+export { default as Fiverr } from "@/public/fiverr.svg"
--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,21 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
