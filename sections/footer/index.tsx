c304e69bb042493f7626f1833ff0a69411c81bca
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-21 11:25:03 +0100

    style(footer): adjust Lenix text layout in footer component
    - Wrap Lenix text in a flex container to center it horizontally
    - Reduce text size from 9xl to 30rem for better scaling and appearance
    - Replace span wrapper with a div to control layout more effectively

--- sections/footer/index.tsx
+++ sections/footer/index.tsx
@@ -9,9 +9,11 @@ const Footer = () => {
     <footer id="footer" className="h-screen flex flex-col items-center justify-evenly">
       <DockDemo/>
       <p className="text-center text-gray-500 text-xs">© {new Date().getFullYear()} {Dict.lenix}. {Dict.rights}.</p>
-      <span className="pointer-events-none bg-linear-to-b from-black to-gray-300/10 bg-clip-text text-center text-9xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10">
-        {Dict.lenix}
-      </span>
+      <div className="w-full flex justify-center text-[30rem]">
+        <span className="pointer-events-none bg-linear-to-b from-black to-gray-300/10 bg-clip-text text-center leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10">
+          {Dict.lenix}
+        </span>
+      </div>
     </footer>
   )
 }
