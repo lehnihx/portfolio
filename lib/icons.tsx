cb810110ae204353e0114dd30b0193a4289143d3
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-23 13:11:29 +0100

    feat(icons): add Lenix icon export
    - Export Lenix SVG icon from public directory
    - Preserve existing icon exports for Grey, Qoder, and Fiverr

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -1,3 +1,4 @@
 export { default as Grey } from "@/public/grey.svg"
 export { default as Qoder } from "@/public/qoder.svg"
 export { default as Fiverr } from "@/public/fiverr.svg"
+export { default as Lenix } from "@/public/lenix.svg"
