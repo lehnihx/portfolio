833e538a5ad03b005e97e82e4e92c6186cd3adb3
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-01 11:33:29 +0100

    refactor(articles): reorganize content and remove unused components
    - Remove lamp-demo.tsx and layout-text-flip-demo.tsx from articles directory
    - Remove hyper-text.tsx and lamp.tsx from stock/ui directory
    - Rename click.tsx and dotted-map.tsx to components/ui directory
    - Add new Experience component with animated text flip feature using motion and LayoutTextFlip
    - Simplify project structure by moving UI components out of stock directory

---
+++ articles/experience.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ANIMATION } from "@/lib/utils";
+import { LayoutTextFlip } from "@/stock/ui/layout-text-flip";
+import { motion } from "motion/react";
+
+export default function Experience() {
+  return (
+    <motion.div {...ANIMATION} className="relative flex-1/4 mx-4 my-4 flex flex-col items-center justify-center gap-4 text-center sm:mx-0 sm:mb-0 sm:flex-row">
+      <LayoutTextFlip
+        text="Did you know that Lenix is "
+        words={["Web Developer", "FiveM Scripter"]}
+        duration={6000}
+      />
+    </motion.div>
+  );
+}
