8c92389be9392591a89de0ac21fdb6aa3b93f4ec
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-23 00:53:40 +0100

    feat/scripts: Filter older commits by one year from repository dates API.

--- scripts/commits.ts
+++ scripts/commits.ts
@@ -2,23 +2,20 @@ import { octokit, ownerRepos, VALID_NAMES } from "./client"
 
 const repoDates = async (owner: string, repo: string) => {
   const dates: string[] = []
-  const pages =
-    octokit.paginate.iterator(octokit.rest.repos.listCommits, { owner, repo, per_page: 100 })
+  const yearBehind = new Date()
+  yearBehind.setFullYear(yearBehind.getFullYear() - 1)
+  const pages = octokit.paginate.iterator(octokit.rest.repos.listCommits, { owner, repo, per_page: 100 })
   for await (const { data } of pages) {
+    let done = false
     for (const { commit } of data) {
-      if (!VALID_NAMES.includes(commit.author?.name ?? '')) continue
-
       if (!commit.author?.date) continue
-
-      const commitDate = new Date(commit.author.date).toLocaleDateString('en-US', {
+      if (new Date(commit.author.date) < yearBehind) { done = true; break }
+      if (!VALID_NAMES.includes(commit.author?.name ?? '')) continue
+      dates.push(new Date(commit.author.date).toLocaleDateString('en-US', {
         year: 'numeric', month: 'short', day: 'numeric'
-      })
-      const yearBehind = new Date()
-      yearBehind.setFullYear(yearBehind.getFullYear() - 1)
-      if (new Date(commit.author.date) < yearBehind) continue
-
-      dates.push(commitDate)
+      }))
     }
+    if (done) break
   }
   return dates
 }
