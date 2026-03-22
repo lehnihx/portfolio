5e3dee4c7a9647d0914b6b674ef859045b3c8a07
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-23 00:53:49 +0100

    feat(main): fix getStats API call and totalLinesAdded data handling.

--- scripts/loc.ts
+++ scripts/loc.ts
@@ -1,26 +1,29 @@
 import { wait } from "lenix"
 import { octokit, ownerRepos } from "./client"
 
-const getStats = async (owner: string, repo: string, retries = 3) => {
-  for (let i = 0; i < retries; i++) {
-    const { data, status } = await octokit.rest.repos.getContributorsStats({ owner, repo })
-    if (status === 200) return data
-    console.warn(`retrying ${owner}/${repo}...`)
-    await wait(3000)
-  }
-  console.warn("failed to get stats for", owner, repo)
-  return []
+const getStats = async (owner: string, repo: string) => {
+  const { data, status } = await octokit.rest.repos.getContributorsStats({ owner, repo })
+  if (status === 200) return data
+
+  console.warn(`retrying ${owner}/${repo}...`)
+  await wait(3000)
+
+  // 😌 consistency is key in life; f*ck it I'm not letting microsoft go 😂
+  return getStats(owner, repo)
 }
 
 export const totalLinesAdded = async () => {
-  let total = 0
+  const total: { added: number, deleted: number } = { added: 0, deleted: 0 }
   for (const { name, owner } of ownerRepos) {
 
     const stats = await getStats(owner.login, name)
     for (const contributor of stats) {
       if (contributor.author?.login !== 'LenixDev') continue
 
-      for (const week of contributor.weeks) total += week.a ?? 0
+      for (const week of contributor.weeks) {
+        total.added += week.a ?? 0
+        total.deleted += week.d ?? 0
+      }
     }
   }
   return total
