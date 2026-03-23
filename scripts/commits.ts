8b4969d52477bc93a1b5f810204bdd7e6375f064
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-24 00:21:52 +0100

    refactor(commits): simplify author destructuring and add eslint max-statements disable

--- scripts/commits.ts
+++ scripts/commits.ts
@@ -1,5 +1,6 @@
 import { octokit, ownerRepos, VALID_NAMES } from "./client"
 
+// eslint-disable-next-line max-statements
 const repoDates = async (owner: string, repo: string) => {
   const dates: string[] = []
   const yearBehind = new Date()
@@ -7,14 +8,14 @@ const repoDates = async (owner: string, repo: string) => {
   const pages = octokit.paginate.iterator(octokit.rest.repos.listCommits, { owner, repo, per_page: 100 })
   for await (const { data } of pages) {
     let done = false
-    for (const { commit } of data)
-    if (typeof commit.author?.date === 'string') {
-      if (new Date(commit.author.date) < yearBehind) {
+    for (const { commit: { author } } of data)
+    if (typeof author?.date === 'string') {
+      if (new Date(author.date) < yearBehind) {
         done = true
         break
       }
-      if (VALID_NAMES.includes(commit.author.name ?? ''))
-      dates.push(new Date(commit.author.date).toLocaleDateString('en-US', {
+      if (VALID_NAMES.includes(author.name ?? ''))
+      dates.push(new Date(author.date).toLocaleDateString('en-US', {
         year: 'numeric', month: 'short', day: 'numeric'
       }))
     }
