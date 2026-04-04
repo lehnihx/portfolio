28608f94a0e0ecde980db7c31eba33608f861fa2
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-04 01:48:54 +0100

    feat(index): add host-based conditional rendering for Contact page

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 import { Layout } from './layout'
 import { DataProvider } from './lib/context'
+import { Contact } from './pages/contact'
+
+const { hostname } = new URL(window.location.href)
+const [host] = hostname.split('.')
+console.log(host, hostname)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <DataProvider>
+    {host === 'lenix' ? <DataProvider>
       <Layout />
     </DataProvider>
+    : host === 'contact' && <Contact />}
   </StrictMode>,
 )
