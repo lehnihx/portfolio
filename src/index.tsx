9cad9a2aafea8c3d40959e83295ac14955e912e8
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-04 02:32:09 +0100

    chore(src): add console.debug to log window.location.hostnamechore(src): add console.debug to log window.location.hostname

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { DataProvider } from './lib/context'
 import { Contact } from './pages/contact'
 
 const isContact = window.location.hostname === 'contact.lenix.dev'
+console.debug(window.location.hostname)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
